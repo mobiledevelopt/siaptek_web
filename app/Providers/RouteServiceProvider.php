5b0a1089b6f334a1bc4475d2f3a322200f37987b
mobiledevelopt <mobiledevlopt@gmail.com>
2025-12-16 20:46:00 +0700

    ok

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -29,7 +29,7 @@ class RouteServiceProvider extends ServiceProvider
         // });
 
         RateLimiter::for('siaptek', function (Request $request) {
-            return Limit::perMinute(500)
+            return Limit::perMinute(1000)
                 // ->by($request->user()?->id ?: $request->ip())
                 ->response(function () {
                     return response()->json(['message' => 'Terlalu banyak permintaan. Harap tunggu selama 1 menit \n'], 429);
