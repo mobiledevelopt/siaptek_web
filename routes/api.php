00fafa30e1274024eff56452e4fe64c6d24b6e25
mobiledevelopt <mobiledevlopt@gmail.com>
2025-12-16 20:09:46 +0700

    ok

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,7 @@ Route::middleware(['auth:sanctum', 'throttle:siaptek'])->get('/user', function (
     return $request->user();
 });
 
-Route::middleware(['auth:sanctum', 'throttle:60,1'])->group(function () {
+Route::middleware(['auth:sanctum', 'throttle:siaptek'])->group(function () {
     Route::post('/logout', [\App\Http\Controllers\Api\AuthController::class, 'logout']);
     
     Route::middleware(['auth:sanctum','throttle:siaptek_post'])->group(function () {
