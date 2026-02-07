406d5ead1ea3a181cef6be80022079f44b9dc160
mobiledevelopt <mobiledevlopt@gmail.com>
2026-02-08 03:27:13 +0700

    ok

--- config/auth.php
+++ config/auth.php
@@ -41,8 +41,9 @@ return [
             'provider' => 'users',
         ],
         'pegawai' => [
-            'driver' => 'jwt',
+            'driver' => 'token',
             'provider' => 'pegawai',
+            'hash' => false,
         ],
         // JWT GUARD (UNTUK API)
         'api' => [
